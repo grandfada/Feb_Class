5100ea7f5bc3160d2f4a79235b3aad5decce17ab
Henry Ayeni <grandfada@henrys-air.home>
2023-03-04 12:07:51 -0500

    Code practice

---
+++ code_prac.py
@@ -0,0 +1,45 @@
+# #mad lib game with input from user
+# color = input("Enter a color: ")
+# plural_noun = input("Enter a plural noun: ")
+# celebrity = input("Enter a celebrity: ")
+#
+# print("Roses are " + color)
+# print(plural_noun + " are cool")
+# #print("I love " + celebrity)
+
+# #Lists
+# friends = ["Sam", "Tobi", "Abbey"]
+# friends[1] = "Vic"
+# print(friends[1])
+
+# #Simple calculator
+# def cal(num1, num2):
+#     if op == "+":
+#         print(num1, "+", num2, " is: ", num1 + num2)
+#     elif op == "-":
+#         print(num1, "-", num2, " is: ", num1 - num2)
+#     elif op == "/":
+#         print(num1, "/", num2, " is: ", num1 / num2)
+#     elif op == "*":
+#         print(num1, "*", num2, " is: ", num1 * num2)
+#     else:
+#         print("Invalid operator")
+#
+# num1 = float(input("Enter first number: "))
+# op = input("Enter operator: ")
+# num2 = float(input("Enter second number: "))
+#
+# cal(num1, num2)
+
+# #while loop
+#
+# i = 1
+# while i <= 20:
+#     print(i)
+#     i += 5
+#
+# print("End of loop")
+
+
+
+
